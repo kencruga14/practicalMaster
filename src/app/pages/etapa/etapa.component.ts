57239ab815a35071241537edeb839fc22c831e84
kencruga <kac.garcia@yavirac.edu.ec>
2021-11-25 22:34:28 -0500

    creacion de objetos para peticiones

--- src/app/pages/etapa/etapa.component.ts
+++ src/app/pages/etapa/etapa.component.ts
@@ -14,68 +14,56 @@ export class EtapaComponent implements OnInit {
   etapas: UsuarioModelo[] = [];
   urbanizaciones: UsuarioModelo[] = [];
   urbanizacionseleccionada: any;
-  base: any;
   id_etapa: 0;
-  fecha_alicuota: "";
-  nombre: "";
+  id: 0;
+  id_urbanizacion: 0;
   nombres: any;
   edit: false;
-  correo: "";
-  telefono: "";
-  imagen = null;
-  id: 0;
-  urbanizacion: boolean;
-  modulo_market: boolean;
-  modulo_publicacion: boolean;
-  modulo_equipo: boolean;
-  modulo_historia: boolean;
-  modulo_bitacora: boolean;
 
-  modulo_alicuota: boolean;
+  nombre: "";
+  correo: "";
+  usuario: any;
+  telefono: "";
+  nombre_banco: "";
+  tipo_cuenta: "";
+  numero_cuenta: "";
   pagos_tarjeta: boolean;
+  modulo_mi_registro: boolean;
+  modulo_alicuota: boolean;
   modulo_emprendimiento: boolean;
   modulo_votacion: boolean;
   modulo_area_social: boolean;
-  modulo_camara: boolean;
+  modulo_camaras: boolean;
   modulo_directiva: boolean;
   modulo_galeria: boolean;
-  modulo_horario: boolean;
-  formularioIngreso: true;
-  formularioSalida: true;
-  usuario: any;
-  tipo_cuenta: "";
-  nombre_banco: "";
-  numero_cuenta: "";
-  tipo_documento: "";
-  numero_documento: "";
-  id_urbanizacion: 0;
-  changeFoto = false;
+  modulo_horarios: boolean;
+  formulario_entrada: boolean;
+  formulario_salida: boolean;
 
   filterName = "";
   etapa = {
-    usuario: "",
     id_etapa: 0,
-    fecha_alicuota: "",
+    edit: "",
+
     nombre: "",
-    edit: false,
-    imagen: null,
-    urbanizacion: "",
     correo: "",
+    usuario: "",
     telefono: "",
-    tipo_cuenta: "",
     nombre_banco: "",
+    tipo_cuenta: "",
     numero_cuenta: "",
-    tipo_documento: "",
-    numero_documento: "",
-    modulo_market: false,
-    modulo_publicacion: false,
-    modulo_votacion: false,
-    modulo_area_social: false,
-    modulo_equipo: false,
-    modulo_historia: false,
-    modulo_bitacora: false,
-    id_urbanizacion: 0,
-    pagos_tarjeta: false,
+    pagos_tarjeta: true,
+    modulo_mi_registro: true,
+    modulo_alicuota: true,
+    modulo_emprendimiento: true,
+    modulo_votacion: true,
+    modulo_area_social: true,
+    modulo_camaras: true,
+    modulo_directiva: true,
+    modulo_galeria: true,
+    modulo_horarios: true,
+    formulario_entrada: true,
+    formulario_salida: true,
   };
   acceso = {
     accesos: "",
@@ -94,30 +82,18 @@ export class EtapaComponent implements OnInit {
     this.getEtapa();
     this.getUrb();
     const id = Number(this.activatedRoute.snapshot.paramMap.get("ID"));
-    this.getLastIdEtapa();
+    // this.getLastIdEtapa();
   }
   getUrb() {
     this.auth.getUrb().subscribe((resp: any) => {
-      console.log(resp);
       this.urbanizaciones = resp;
+      console.log("urbanizaciones: ", this.urbanizaciones);
     });
   }
+
   openAcceso(content, acceso) {
     this.acceso.id_etapa = acceso.id_etapa;
     this.modalService.open(content);
-  }
-  preview(event: any) {
-    const fileData = event.target.files[0];
-    const mimeType = fileData.type;
-    if (mimeType.match(/image\/*/) == null) {
-      return;
-    }
-    const reader = new FileReader();
-    reader.readAsDataURL(fileData);
-    reader.onload = (response) => {
-      this.imagen = reader.result;
-    };
-    this.changeFoto = true;
   }
 
   openInfo(content) {
@@ -126,136 +102,144 @@ export class EtapaComponent implements OnInit {
 
   openEtapa(content, etapa = null) {
     if (etapa) {
+      console.log("etapa admin: ", etapa);
+      this.etapa.edit = "0";
+      this.id_urbanizacion = etapa.id_urbanizacion;
       this.id_etapa = etapa.ID;
-      this.id = etapa.ID;
-      this.correo = etapa.correo;
-      this.telefono = etapa.telefono;
       this.nombre = etapa.nombre;
-      this.etapa.edit = true;
-      this.fecha_alicuota = etapa.fecha_alicuota;
-      this.imagen = null;
-      this.tipo_cuenta = etapa.tipo_cuenta;
+      this.correo = etapa.correo;
+      // this.usuario = etapa.usuario;
+      this.telefono = etapa.telefono;
       this.nombre_banco = etapa.nombre_banco;
+      this.tipo_cuenta = etapa.tipo_cuenta;
       this.numero_cuenta = etapa.numero_cuenta;
-      this.tipo_documento = etapa.tipo_documento;
-      this.numero_documento = etapa.numero_documento;
-      this.modulo_market = etapa.modulo_market;
-      this.modulo_publicacion = etapa.modulo_publicacion;
+      this.pagos_tarjeta = etapa.pagos_tarjeta;
+      this.modulo_mi_registro = etapa.modulo_mi_registro;
+      this.modulo_alicuota = etapa.modulo_alicuota;
+      this.modulo_emprendimiento = etapa.modulo_emprendimiento;
       this.modulo_votacion = etapa.modulo_votacion;
       this.modulo_area_social = etapa.modulo_area_social;
-      this.modulo_equipo = etapa.modulo_equipo;
-      this.modulo_historia = etapa.modulo_historia;
-      this.modulo_bitacora = etapa.modulo_bitacora;
-      this.id_urbanizacion = etapa.id_urbanizacion;
-      this.pagos_tarjeta = etapa.pagos_tarjeta;
+      this.modulo_camaras = etapa.modulos_camaras;
+      this.modulo_directiva = etapa.modulo_directiva;
+      this.modulo_galeria = etapa.modulo_galeria;
+      this.modulo_horarios = etapa.modulo_horarios;
+      this.formulario_entrada = etapa.formulario_entrada;
+      this.formulario_salida = etapa.formulario_salida;
     } else {
+      this.id_urbanizacion = 0;
       this.id_etapa = 0;
       this.nombre = "";
-      this.usuario = "";
       this.correo = "";
+      // this.usuario = "";
       this.telefono = "";
-      this.nombre = "";
-      this.id_urbanizacion = 0;
-      this.tipo_cuenta = "";
       this.nombre_banco = "";
+      this.tipo_cuenta = "";
       this.numero_cuenta = "";
-      this.tipo_documento = "";
-      this.numero_documento = "";
-      this.etapa.edit = false;
-      this.fecha_alicuota = "";
-      this.modulo_market = false;
-      this.modulo_publicacion = false;
-      this.modulo_votacion = false;
-      this.modulo_area_social = false;
-      this.modulo_equipo = false;
-      this.modulo_historia = false;
-      this.pagos_tarjeta = false;
-      this.modulo_bitacora = false;
-      this.formularioIngreso = true;
-      this.formularioSalida = true;
-      this.imagen = null;
+      this.pagos_tarjeta = true;
+      this.modulo_mi_registro = true;
+      this.modulo_alicuota = true;
+      this.modulo_emprendimiento = true;
+      this.modulo_votacion = true;
+      this.modulo_area_social = true;
+      this.modulo_camaras = true;
+      this.modulo_directiva = true;
+      this.modulo_galeria = true;
+      this.modulo_horarios = true;
+      this.formulario_entrada = true;
+      this.formulario_salida = true;
     }
     this.modalService.open(content);
   }
+
   getEtapa() {
     this.auth.getEtapa().subscribe((resp: any) => {
       console.log(resp);
       this.etapas = resp;
-      this.base = resp;
-      console.log("base; ", this.etapas);
+      console.log("etapas: ", this.etapas);
     });
   }
 
-  getLastIdEtapa() {
-    var output = [];
-    var vals=[];
-    for(var item of this.base){
-       vals.push(item.ID); 
-    }
-    console.log("valor: ", vals)
-    // return output;
-}
-    // return suma;
+  // getLastIdEtapa() {
+  //   var output = [];
+  //   var vals = [];
+  //   for (var item of this.base) {
+  //     vals.push(item.ID);
+  //   }
+  //   console.log("valor: ", vals);
+  //   // return output;
+  // }
+  // return suma;
   // }
 
   async gestionEtapa() {
     let response: any;
-    if (this.etapa.edit) {
+    if (this.etapa.edit === "0") {
+      console.log("entro a etapa edit:");
       const body = {
         id_urbanizacion: this.id_urbanizacion,
         nombre: this.nombre,
-        usuario: this.usuario,
+        correo: this.correo,
+        // usuario: this.usuario,
+        telefono: this.telefono,
+        nombre_banco: this.nombre_banco,
+        tipo_cuenta: this.tipo_cuenta,
+        numero_cuenta: this.numero_cuenta,
+        pagos_tarjeta: this.pagos_tarjeta,
+        modulo_mi_registro: this.modulo_mi_registro,
+        modulo_alicuota: this.modulo_alicuota,
+        modulo_emprendimiento: this.modulo_emprendimiento,
+        modulo_votacion: this.modulo_votacion,
+        modulo_area_social: this.modulo_area_social,
+        modulo_camaras: this.modulo_camaras,
+        modulo_directiva: this.modulo_directiva,
+        modulo_galeria: this.modulo_galeria,
+        modulo_horarios: this.modulo_horarios,
+        formulario_entrada: this.formulario_entrada,
+        formulario_salida: this.formulario_salida,
+      };
+      console.log("modificar etapa: ", body);
+      response = await this.auth.editEtapa(this.id_etapa, body);
+    } else {
+      console.log("entro a crear:");
+      const body = {
+        id_urbanizacion: this.id_urbanizacion,
+        nombre: this.nombre,
+        // usuario: this.usuario,
         correo: this.correo,
         telefono: this.telefono,
         nombre_banco: this.nombre_banco,
         tipo_cuenta: this.tipo_cuenta,
         numero_cuenta: this.numero_cuenta,
-
-        modulo_mi_registro: this.modulo_market,
-        formulario_entrada: this.modulo_publicacion,
-        formulario_salida: this.modulo_votacion,
-        pagos_tarjeta: this.modulo_area_social,
-        modulo_alicuota: this.modulo_equipo,
-        modulo_emprendimiento: this.modulo_historia,
-        modulo_votacion: this.modulo_bitacora,
-        modulo_area_social: this.pagos_tarjeta,
-      };
-      console.log("modificar etapa: ", body);
-      // response = await this.auth.editEtapa(this.id, body);
-    } else {
-      const body = {
-        id_urbanizacion: this.id_urbanizacion,
-        nombre: this.nombre,
-        usuario: this.usuario,
-        correo: this.correo,
-        telefono: this.telefono,
-        nombre_banco: this.nombre_banco,
-
+        pagos_tarjeta: this.pagos_tarjeta,
         modulo_mi_registro: true,
-        formulario_entrada: true,
-        formulario_salida: true,
-        pagos_tarjeta: true,
         modulo_alicuota: true,
         modulo_emprendimiento: true,
         modulo_votacion: true,
         modulo_area_social: true,
+        modulo_camaras: true,
+        modulo_directiva: true,
+        modulo_galeria: true,
+        modulo_horarios: true,
+        formulario_entrada: true,
+        formulario_salida: true,
       };
-      console.log("modificar etapa: ", body);
-      let lastId = this.getLastIdEtapa();
-      console.log("last Id: ", lastId);
-      let datosUsuarios = {
-        id_etapa: lastId,
-        cedula: null,
-        usuario: {
-          nombres: this.nombre,
-          usuario: this.usuario,
-          correo: this.correo,
-          telefono: this.telefono,
-        },
-      };
+      console.log("crear etapa: ", body);
+      response = await this.auth.createEtapa(body);
 
-      this.createUser(datosUsuarios);
-      // response = await this.auth.createEtapa(body);
+      // let lastId = this.getLastIdEtapa();
+      // console.log("last Id: ", lastId);
+      // let datosUsuarios = {
+      //   id_etapa: lastId,
+      //   cedula: null,
+      //   usuario: {
+      //     nombres: this.nombre,
+      //     usuario: this.usuario,
+      //     correo: this.correo,
+      //     telefono: this.telefono,
+      //   },
+      // };
+
+      // this.createUser(datosUsuarios);
     }
     if (response) {
       // let usuario = {
@@ -272,7 +256,7 @@ export class EtapaComponent implements OnInit {
   }
 
   createUser(objeto: any) {
-    console.log("Datos del usuario a crear: ", objeto);
+    // console.log("Datos del usuario a crear: ", objeto);
   }
 
   delete(id: number) {
