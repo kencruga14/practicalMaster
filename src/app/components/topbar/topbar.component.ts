f38779d9cac3fd35e9bd894772f0e181c593b1ee
juanjose loor toala <juanjoloor@hotmail.com>
2021-12-22 11:54:31 -0500

    correciones

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { UsuarioModelo } from 'src/app/models/usuario.model';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-topbar',
@@ -22,8 +23,28 @@ masters: UsuarioModelo[] =[];
     console.log("masters: ", this.masters)
   }
 
+  // logout() {
+  //   this.auth.logout();
+  //   this.router.navigateByUrl('/login');
+  // }
+
+
   logout() {
-    this.auth.logout();
-    this.router.navigateByUrl('/login');
+    Swal.fire({
+      title: "¿Seguro que quiere salir ?",
+      showCancelButton: true,
+      confirmButtonColor: "#343A40",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.value) {
+        // setTimeout(() => {this.auth.logout(),5000})
+        this.auth.logout();
+        this.router.navigateByUrl('/login');
+      }
+    });
+    
   }
+
 }
